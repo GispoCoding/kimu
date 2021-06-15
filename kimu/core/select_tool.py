4fbff50ef5a992662331548727c23e0fbb70b40e
Mikael Vaaltola <mvaaltola@gmail.com>
2021-06-15 12:23:43 +0300

    Explode polygons to lines

--- kimu/core/select_tool.py
+++ kimu/core/select_tool.py
@@ -1,6 +1,13 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QCursor
-from qgis.core import QgsVectorLayer
+from qgis import processing
+from qgis.core import (
+    QgsFeatureRequest,
+    QgsProcessingFeatureSourceDefinition,
+    QgsProject,
+    QgsVectorLayer,
+    QgsWkbTypes,
+)
 from qgis.gui import (
     QgisInterface,
     QgsMapCanvas,
@@ -30,27 +37,42 @@ class SelectTool(QgsMapToolIdentifyFeature):
 
     def active_changed(self, layer: QgsVectorLayer) -> None:
         """Triggered when active layer changes."""
-        try:
-            self.layer.removeSelection()
-
-        except AttributeError:
-            pass
-        except RuntimeError:
-            pass
-
-        if isinstance(layer, QgsVectorLayer) and layer.isSpatial():
+        if (
+            isinstance(layer, QgsVectorLayer)
+            and layer.isSpatial()
+            and layer.geometryType() == QgsWkbTypes.PolygonGeometry
+        ):
             self.layer = layer
             self.setLayer(self.layer)
 
     def canvasPressEvent(self, event: QgsMapMouseEvent) -> None:  # noqa: N802
         LOGGER.info("canvas pressed")
+        if self.iface.activeLayer() != self.layer:
+            LOGGER.info("Please select a polygon layer")
+            return
         found_features = self.identify(
-            event.x(), event.y(), [self.layer], QgsMapToolIdentify.TopDownAll
+            event.x(), event.y(), [self.layer], QgsMapToolIdentify.ActiveLayer
         )
         self.layer.selectByIds(
-            [f.mFeature.id() for f in found_features], QgsVectorLayer.AddToSelection
+            [f.mFeature.id() for f in found_features], QgsVectorLayer.SetSelection
         )
 
+        line_params = {
+            "INPUT": QgsProcessingFeatureSourceDefinition(
+                self.layer.id(),
+                selectedFeaturesOnly=True,
+                featureLimit=-1,
+                geometryCheck=QgsFeatureRequest.GeometryAbortOnInvalid,
+            ),
+            "OUTPUT": "memory:",
+        }
+        line_result = processing.run("native:polygonstolines", line_params)
+        line_layer = line_result["OUTPUT"]
+
+        explode_params = {"INPUT": line_layer, "OUTPUT": "memory:"}
+        explode_result = processing.run("native:explodelines", explode_params)
+        explode_layer = explode_result["OUTPUT"]
+        QgsProject.instance().addMapLayer(explode_layer)
+
     def deactivate(self) -> None:
-        self.layer.removeSelection()
         self.action().setChecked(False)
