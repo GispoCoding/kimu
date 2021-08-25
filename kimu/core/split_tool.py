d9f85b2163b445b30a49f2e69f928efec2be6280
Mikael Vaaltola <mvaaltola@gmail.com>
2021-08-25 14:58:27 +0300

    Refactor split tool, apply color scheme to layer

--- kimu/core/split_tool.py
+++ kimu/core/split_tool.py
@@ -3,10 +3,14 @@ from typing import List
 from PyQt5.QtCore import Qt
 from qgis import processing
 from qgis.core import (
+    QgsCategorizedSymbolRenderer,
     QgsFeatureRequest,
     QgsGeometry,
+    QgsLineSymbol,
     QgsProcessingFeatureSourceDefinition,
     QgsProject,
+    QgsRendererCategory,
+    QgsStyle,
     QgsVectorLayer,
     QgsWkbTypes,
 )
@@ -46,10 +50,28 @@ class SplitTool(SelectTool):
             self.setLayer(self.layer)
 
     def canvasPressEvent(self, event: QgsMapMouseEvent) -> None:  # noqa: N802
-        """Identify clicked line feature and split it to N parts."""
+        """Handles split tool canvas click event.
+        Splits line to N parts, extracts vertices."""
         if self.iface.activeLayer() != self.layer:
             LOGGER.warning(tr("Please select a line layer"), extra={"details": ""})
             return
+
+        geometry = self._identify_and_extract_single_geometry(event)
+        if geometry.isEmpty():  # No geometry identified
+            return
+
+        split_line_layer = self._create_split_line_layer(geometry)
+        self._set_split_layer_style(split_line_layer)
+        QgsProject.instance().addMapLayer(split_line_layer)
+
+        vertex_layer = self._create_vertex_layer(split_line_layer)
+        QgsProject.instance().addMapLayer(vertex_layer)
+
+    def _identify_and_extract_single_geometry(
+        self, event: QgsMapMouseEvent
+    ) -> QgsGeometry:
+        """Identifies clicked feature and extracts its geometry.
+        Returns empty geometry if nr. of identified features != 1."""
         found_features: List[QgsMapToolIdentify.IdentifyResult] = self.identify(
             event.x(), event.y(), [self.layer], QgsMapToolIdentify.ActiveLayer
         )
@@ -58,16 +80,18 @@ class SplitTool(SelectTool):
                 tr("Please select one line"), extra={"details": "", "duration": 1}
             )
             self.ui.set_split_length(0)
-            return
+            return QgsGeometry()
         self.layer.selectByIds(
             [f.mFeature.id() for f in found_features], QgsVectorLayer.SetSelection
         )
         geometry: QgsGeometry = found_features[0].mFeature.geometry()
+        return geometry
 
+    def _create_split_line_layer(self, geometry: QgsGeometry) -> QgsVectorLayer:
+        """Create a QgsVectorLayer containing line split to N parts of equal length."""
         split_to = self.ui.get_split_parts()
         split_length = geometry.length() / split_to
         self.ui.set_split_length(split_length)
-
         split_params = {
             "INPUT": QgsProcessingFeatureSourceDefinition(
                 self.layer.id(),
@@ -79,17 +103,33 @@ class SplitTool(SelectTool):
             "OUTPUT": "memory:",
         }
         split_result = processing.run("native:splitlinesbylength", split_params)
-        split_layer = split_result["OUTPUT"]
+        split_layer: QgsVectorLayer = split_result["OUTPUT"]
         split_layer.setName(tr("Split line"))
-        split_layer.renderer().symbol().setWidth(2)
-        # TODO: set visualization to categorized with expression $id
-        QgsProject.instance().addMapLayer(split_layer)
+        return split_layer
 
+    @staticmethod
+    def _set_split_layer_style(split_layer: QgsVectorLayer) -> None:
+        """Set a style to given layer making line segments easy to distinguish."""
+        split_layer_ids = [feature.id() for feature in split_layer.getFeatures()]
+        categories = []
+        for id_ in split_layer_ids:
+            symbol = QgsLineSymbol.createSimple(properties={"width": 2})
+            category = QgsRendererCategory(id_, symbol, str(id_))
+            categories.append(category)
+        renderer = QgsCategorizedSymbolRenderer("$id", categories)
+        ramp = QgsStyle().defaultStyle().colorRamp("Turbo")
+        renderer.updateColorRamp(ramp)
+        split_layer.setRenderer(renderer)
+
+    @staticmethod
+    def _create_vertex_layer(split_layer: QgsVectorLayer) -> QgsVectorLayer:
+        """Extracts start and end vertices from each line segment.
+        Returns a point layer."""
         extract_result = processing.run(
             "native:extractspecificvertices",
             {"INPUT": split_layer, "VERTICES": "0,-1", "OUTPUT": "memory:"},
         )
-        extract_layer = extract_result["OUTPUT"]
-        extract_layer.setName(tr("Nodes"))
-        extract_layer.renderer().symbol().setSize(3)
-        QgsProject.instance().addMapLayer(extract_layer)
+        vertex_layer = extract_result["OUTPUT"]
+        vertex_layer.setName(tr("Nodes"))
+        vertex_layer.renderer().symbol().setSize(3)
+        return vertex_layer
