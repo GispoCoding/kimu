9db2ca7c34ac363f26eff8a6c0e77a2a53d141e7
Mikael Vaaltola <mvaaltola@gmail.com>
2021-09-08 11:10:45 +0300

    Add unit test for splitting line layers

--- kimu/test/test_split_tool.py
+++ kimu/test/test_split_tool.py
@@ -1,11 +1,33 @@
+import pytest
 from qgis.core import (
     QgsCategorizedSymbolRenderer,
     QgsGradientColorRamp,
     QgsLineSymbol,
     QgsStyle,
+    QgsVectorLayer,
 )
 
 from kimu.core.split_tool import SplitTool
+from kimu.qgis_plugin_tools.tools.i18n import tr
+from kimu.ui.split_tool_dockwidget import SplitToolDockWidget
+
+
+@pytest.fixture
+def split_tool(qgis_iface, mocker):
+    dock_widget = SplitToolDockWidget(qgis_iface)
+    processing_mock = mocker.patch("kimu.core.split_tool.processing")
+    processing_mock.run.return_value = {"OUTPUT": QgsVectorLayer()}
+    split_tool = SplitTool(qgis_iface, dock_widget)
+    yield split_tool
+
+
+def test_create_split_line_layer(layer_lines_arcs, split_tool):
+    features = [feature for feature in layer_lines_arcs.getFeatures()]
+    geometry = features[0].geometry()
+    split_tool.layer = layer_lines_arcs
+    result = split_tool._create_split_line_layer(geometry)
+    assert type(result) == QgsVectorLayer
+    assert result.name() == tr("Split line")
 
 
 def test_color_ramp():
