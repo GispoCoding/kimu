1eb7c99a6375f13c42023d09e3cd14993bceb278
pavetsu14 <pauliina@gispo.fi>
2022-03-04 11:59:28 +0200

    Second attempt to ignore flake8 for one line

--- kimu/core/intersection_tool_line_circle.py
+++ kimu/core/intersection_tool_line_circle.py
@@ -40,7 +40,9 @@ class IntersectionLineCircle(SelectTool):
             self.layer = layer
             self.setLayer(self.layer)
 
-    def canvasPressEvent(self, event: QgsMapToolEmitPoint) -> None:  # fmt: skip noqa: N802
+    def canvasPressEvent(
+        self, event: QgsMapToolEmitPoint
+    ) -> None:  # noqa: N802
         """Canvas click event for storing centroid point of the circle."""
         if self.iface.activeLayer() != self.layer:
             LOGGER.warning(tr("Please select a line layer"), extra={"details": ""})
